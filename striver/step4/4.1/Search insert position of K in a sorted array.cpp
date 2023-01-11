b3265ac208d4f438dd93c4b5e54bd93fe0cb235c
poojaramnaney <poojaramnaney@gmail.com>
2023-01-12 01:47:45 +0530

    day 3 DSA

---
+++ striver/step4/4.1/Search insert position of K in a sorted array.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int searchInsertK(vector<int>Arr, int N, int k)
+    {
+        for(int i=0; i<N; i++){
+            
+            if(Arr[i]>=k){
+                return i;
+            }
+        }
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        vector<int>Arr(N);
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.searchInsertK(Arr, N, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
