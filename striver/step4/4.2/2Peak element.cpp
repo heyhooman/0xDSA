3d0e9cf5515832ddf241c56c95f1ab78410db5a8
poojaramnaney <poojaramnaney@gmail.com>
2023-01-13 00:58:56 +0530

    day 4// 25 26 27

---
+++ striver/step4/4.2/2Peak element.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+        for(int i=1;i<n-1;i++){
+           if(arr[i]>=arr[i-1] && arr[i]>=arr[i+1]) {
+               return i;
+           }
+       }
+       
+       if(arr[0] >= arr[n-1]) return 0;
+       return n-1;
+       // Your code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
+// } Driver Code Ends
