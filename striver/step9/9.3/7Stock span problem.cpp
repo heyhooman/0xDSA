bb9e9e8c82ccd8cc096e2f0a9425f7063b1bed5f
poojaramnaney <poojaramnaney@gmail.com>
2023-01-18 00:19:37 +0530

    day 8//7

---
+++ striver/step9/9.3/7Stock span problem.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       vector<int> res;
+       stack<pair<int,int>> s;   
+       for(int i = 0 ; i < n ; i++) {
+           int ans = 1;
+           while(!s.empty() && s.top().first <= price[i]) {
+                ans += s.top().second;
+                s.pop();
+            }
+            s.push({price[i],ans});
+            res.push_back(ans);
+       }
+       return res;
+ 
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
