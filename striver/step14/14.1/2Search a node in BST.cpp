df0c59f343f21135acb1d117c3dd42671cbaaecb
poojaramnaney <poojaramnaney@gmail.com>
2023-01-21 00:11:56 +0530

    day 11//11

---
+++ striver/step14/14.1/2Search a node in BST.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+bool search(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << search(root, s);
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+bool search(Node* root, int x) {
+    // Your code here
+    if(root==NULL){
+        return 0;
+    }
+    if(root->data==x){
+        return 1;
+    }
+    if(x<root->data){
+        return search(root->left,x);
+    }
+    else if(x>=root->data){
+       return search(root->right,x);
+
+    }
+}
