3d6d880a50aec2fdd0129453c1972419c6904d01
poojaramnaney <poojaramnaney@gmail.com>
2023-01-21 17:26:29 +0530

    day 12//2

---
+++ striver/step15/15.1/2Print adjacency list.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<vector<int>> x;
+        for(int i=0; i<V; i++){
+            vector<int> temp;
+            temp.push_back(i);
+            for(auto j : adj[i]){
+                temp.push_back(j);
+            }
+            
+            x.push_back(temp);
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        vector<vector<int>> ans = obj.printGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            for (int j = 0; j < ans[i].size() - 1; j++) {
+                cout << ans[i][j] << "-> ";
+            }
+            cout << ans[i][ans[i].size() - 1];
+            cout << endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
