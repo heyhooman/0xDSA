060ec7f5a7d10d4aadb00ecc7816b2869e097491
poojaramnaney <poojaramnaney@gmail.com>
2023-01-27 23:39:26 +0530

    day 18

---
+++ leetcode/Easy/125leetcode.cpp
@@ -0,0 +1,25 @@
+/* 125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+ */
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+     int n = s.size(), l = 0, r = n - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) {
+                l++;
+            }
+            while (l < r && !isalnum(s[r])){
+                r--;
+            }
+            if (tolower(s[l++]) != tolower(s[r--])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
