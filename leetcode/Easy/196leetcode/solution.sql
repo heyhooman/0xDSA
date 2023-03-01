1e9254d178ceec3078653a5bf51e27d7768a0dd8
poojaramnaney <poojaramnaney@gmail.com>
2023-03-01 23:54:16 +0530

    day 42

---
+++ leetcode/Easy/196leetcode/solution.sql
@@ -0,0 +1,3 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+delete A from Person A, Person B where A.id > B.id and A.email=B.email;
